d48388b6a8da1589f2f637021ca45d6e49510795
Oscar <oscargelin@gmail.com>
2023-12-23 21:14:40 +0100

    remove react imports

--- src/assets/images/svgs/PinterestSvg.tsx
+++ src/assets/images/svgs/PinterestSvg.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const PinterestSvg = () => {
   return (
     <svg
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./Image.module.css";
 
 type TImage = {
